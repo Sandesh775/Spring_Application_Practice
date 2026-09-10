45a03b82bba37e88f16a1666138a3f56eee2e682
sandesh <sandeshchapagain99@gmail.com>
2026-09-10 21:10:40 +0545

    JWT mini exercise done !

--- JWT-Mini-Exercise/src/main/java/com/example/JWT/Mini/Exercise/JwtUtil.java
+++ JWT-Mini-Exercise/src/main/java/com/example/JWT/Mini/Exercise/JwtUtil.java
@@ -1,12 +1,36 @@
 package com.example.JWT.Mini.Exercise;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtUtil {
 
-    public String generateToken(String username){
-        return "";
+    private final String SECRET = "ad165b11320bc91501ab08613cc3a48a62a6caca4d5c8b14ca82cc313b3b96cd";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
     }
 
-    public boolean validateToken(){
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(SECRET).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
 
+    public boolean validateToken(String token) {
+        try {
+            extractUsername(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
